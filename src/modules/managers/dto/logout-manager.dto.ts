6bf6e690efca23237bb7553a532ff42567fe83c0
Marika (Jitera) <developers@jitera.com>
2024-02-13 05:19:41 +0000

    Commit from Jitera

--- src/modules/managers/dto/logout-manager.dto.ts
+++ src/modules/managers/dto/logout-manager.dto.ts
@@ -1,9 +1,12 @@
-import { IsString, IsIn } from 'class-validator';
+import { IsString, IsIn, IsNotEmpty } from 'class-validator';
 
 export class LogoutManagerRequest {
   @IsString()
+  @IsNotEmpty()
   token: string;
 
+  @IsString()
   @IsIn(['access_token', 'refresh_token'])
+  @IsNotEmpty()
   token_type_hint: string;
 }
