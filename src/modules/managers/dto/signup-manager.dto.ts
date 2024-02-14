310a7d64f9930464b0991815e6fd6ce272147a0b
Shandie (Jitera) <developers@jitera.com>
2024-02-14 07:44:38 +0000

    Commit from Jitera

--- src/modules/managers/dto/signup-manager.dto.ts
+++ src/modules/managers/dto/signup-manager.dto.ts
@@ -12,9 +12,7 @@ export class SignupManagerRequest {
   @IsNotEmpty({ message: 'Password is required' })
   password: string;
 
-  @IsString({ message: 'Password confirmation must be a string' })
-  @IsNotEmpty({ message: 'Password confirmation is required' })
-  password_confirmation: string;
+  // Removed the password_confirmation field as it is not mentioned in the requirement for the SignupManagerRequest DTO
 }
 
 export class SignupManagerResponse {
