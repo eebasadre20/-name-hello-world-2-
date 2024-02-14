70536ab4b6af8eb57875efb2ebfc8ba84b3972a6
Teri (Jitera) <developers@jitera.com>
2024-02-14 07:46:39 +0000

    Commit from Jitera

---
+++ src/modules/managers/dto/refresh-token.dto.ts
@@ -0,0 +1,54 @@
+import { IsString, IsNotEmpty, IsOptional } from 'class-validator';
+
+export class RefreshTokenRequest {
+  @IsString()
+  @IsNotEmpty()
+  refresh_token: string;
+
+  @IsString()
+  @IsNotEmpty()
+  scope: string;
+
+  @IsString()
+  @IsOptional()
+  client_id?: string;
+
+  @IsString()
+  @IsOptional()
+  client_secret?: string;
+}
+
+export class RefreshTokenResponse {
+  @IsString()
+  @IsNotEmpty()
+  access_token: string;
+
+  @IsString()
+  @IsNotEmpty()
+  refresh_token: string;
+
+  @IsString()
+  @IsNotEmpty()
+  resource_owner: string;
+
+  @IsString()
+  @IsNotEmpty()
+  resource_id: string;
+
+  @IsNotEmpty()
+  expires_in: number;
+
+  @IsString()
+  @IsNotEmpty()
+  token_type: string;
+
+  @IsString()
+  @IsNotEmpty()
+  scope: string;
+
+  @IsNotEmpty()
+  created_at: string;
+
+  @IsNotEmpty()
+  refresh_token_expires_in: number;
+}
