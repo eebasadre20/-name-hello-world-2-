98e74ec1e615d73f1e6e1b051283b8cda9de9bd5
Leona (Jitera) <developers@jitera.com>
2024-02-15 06:24:12 +0000

    feat[service]: Managers signup with email

---
+++ src/modules/managers/dto/signup-manager.dto.ts
@@ -0,0 +1,19 @@
+import { IsEmail, IsString, MinLength, Matches, IsNotEmpty } from 'class-validator';
+import { Manager } from '../../entities/managers.ts'; // Import the Manager entity
+
+export class SignupManagerRequest {
+  @IsEmail({}, { message: 'Invalid email format' })
+  @IsNotEmpty({ message: 'Email is required' })
+  email: string;
+
+  @IsString({ message: 'Password must be a string' })
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  @Matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, { message: 'Password must contain at least one letter and one number' })
+  @IsNotEmpty({ message: 'Password is required' })
+  password: string;
+
+}
+
+export class SignupManagerResponse {
+  user: Manager; // Specify the type as Manager
+}
