14a0799e6489ad348c5041ff8fdad61001e98927
Malva (Jitera) <developers@jitera.com>
2024-02-15 04:06:22 +0000

    Commit from Jitera

---
+++ src/modules/managers/managers.service.ts
@@ -0,0 +1,64 @@
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Manager } from 'src/entities/managers';
+import { SignupManagerRequest, SignupManagerResponse } from './dto/signup-manager.dto';
+import { ConfirmEmailRequest, ConfirmEmailResponse } from './dto/confirm-email.dto';
+import { sendConfirmationEmail } from './utils/email.util';
+import * as bcrypt from 'bcrypt';
+import { randomBytes } from 'crypto';
+
+@Injectable()
+export class ManagersService {
+  constructor(
+    @InjectRepository(Manager)
+    private managersRepository: Repository<Manager>,
+  ) {}
+
+  async signupWithEmail(request: SignupManagerRequest): Promise<SignupManagerResponse> {
+    const { email, password } = request;
+
+    const existingManager = await this.managersRepository.findOne({ where: { email } });
+    if (existingManager) {
+      throw new BadRequestException('Email is already taken');
+    }
+
+    const confirmationToken = randomBytes(32).toString('hex');
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const manager = this.managersRepository.create({
+      email,
+      password: hashedPassword,
+      confirmation_token: confirmationToken,
+      confirmed_at: null,
+    });
+    await this.managersRepository.save(manager);
+
+    const confirmationUrl = `http://yourfrontend.com/confirm-email?confirmation_token=${confirmationToken}`;
+    await sendConfirmationEmail(email, confirmationToken, confirmationUrl);
+
+    return { user: manager };
+  }
+
+  async confirmEmail(request: ConfirmEmailRequest): Promise<ConfirmEmailResponse> {
+    const { token } = request;
+    const manager = await this.managersRepository.findOne({
+      where: {
+        confirmation_token: token,
+        confirmed_at: null,
+      },
+    });
+
+    if (!manager) {
+      throw new BadRequestException('Confirmation token is not valid');
+    }
+
+    manager.confirmed_at = new Date();
+    await this.managersRepository.save(manager);
+
+    return { user: manager };
+  }
+
+  // Placeholder for other service methods
+}
